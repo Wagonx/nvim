ff71432212113733e2e218c114ba590748afaee4
unknown <blombergc@gnty.com>
2025-08-07 13:02:48 -0500

    Fixed PWSH LSP

--- init.lua
+++ init.lua
@@ -3,13 +3,6 @@ require("config.lazy")
 
 vim.lsp.set_log_level("debug")
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "ps1",
-  callback = function()
-    print("PowerShell file opened - LSP disabled for testing")
-  end,
-})
-
 vim.filetype.add({
   extension = {
     ps1 = "ps1",
@@ -18,23 +11,4 @@ vim.filetype.add({
   },
 })
 
--- Increase updatetime to reduce frequent LSP calls
-vim.opt.updatetime = 300 -- Default is often 4000, but 300 is a good balance
 
--- Reduce completion menu update frequency
-vim.opt.timeoutlen = 500
-
--- For PowerShell files specifically
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "ps1",
-  callback = function()
-    -- Disable real-time diagnostics updates
-    vim.diagnostic.config({
-      update_in_insert = false, -- Don't update diagnostics while typing
-      virtual_text = {
-        spacing = 2,
-        prefix = "●",
-      },
-    })
-  end,
-})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,54 +1,90 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "powershell_es" }
-      })
 
-      local lspconfig = require("lspconfig")
-      
-      -- PowerShell LSP configuration
-    lspconfig.powershell_es.setup({
-    bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
-    settings = {
-        cmd = { "pwsh", "-NoLogo", "-NoProfile", "-Command", "..." }, -- Use pwsh instead of powershell
-        powershell = {
-        -- Reduce analysis features that cause lag
-        scriptAnalysis = {
-            enable = true,
-            settingsPath = "", -- Disable custom PSScriptAnalyzer rules
-        },
-        -- Disable code lens (can be performance-heavy)
-        codeLens = {
-            enable = false,
-        },
-        -- Reduce completion trigger characters
-        completion = {
-            completionInvocationTimeout = 2000, -- 2 seconds instead of default 5
-        },
-        -- Disable workspace symbol search for better performance
-        workspaceSymbols = {
-            enable = false,
-        },
-        },
+
+
+
+return {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+        -- "rafamadriz/friendly-snippets",
     },
-    on_attach = function(client, bufnr)
-        -- Disable semantic tokens if they cause lag
-        client.server_capabilities.semanticTokensProvider = nil
-        
-        -- Your keymaps here
-        local opts = { buffer = bufnr, remap = false }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    end,
-    })
-    end,
-  }
+
+    config = function()
+        local cmp = require('cmp')
+        local cmp_lsp = require('cmp_nvim_lsp')
+        local capabilities = vim.tbl_deep_extend(
+            'force',
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+        require('fidget').setup({})
+        require('mason').setup()
+        require('mason-lspconfig').setup({
+            ensure_installed = {},
+            handlers = {
+                function(server_name)
+                    require('lspconfig')[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+                powershell_es = function()
+                    local lspconfig = require('lspconfig')
+                    lspconfig.powershell_es.setup{
+                        bundle_path = '%LOCALAPPDATA%/nvim/lua/plugins/PES:q',
+                        on_attach = function(client, bufnr)
+                            vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+                        end,
+                        settings = {powershell = { codeFormatting = { Preset = 'OTBS'} } }
+                    }
+                end
+            }
+        })
+
+    
+        -- require("luasnip.loaders.from_vscode").lazy_load()
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Replace }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(cmp_select), {'i'}),
+                ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(cmp_select), {'i'}),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-Space>'] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        vim.diagnostic.config({
+            float = {
+                focusable = false,
+                style = 'minimal',
+                border = 'rounded',
+                source = 'always',
+                header = '',
+                prefix = '',
+            },
+        })
+    end
 }
